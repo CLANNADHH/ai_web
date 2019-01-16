0368d615738a02b3d6dea45fe35fe73291f13bc3
HH <clannadhh@gmail.com>
2019-01-17 00:14:17 +0800

    good night

--- aiweb/app.py
+++ aiweb/app.py
@@ -5,6 +5,9 @@ import logging
 from klein import Klein
 from twisted.internet import reactor, threads
 from aiweb.utils.ai_wraper import check_cors
+from aiweb.utils.ai_wraper import check_route
+from aiweb.utils.ai_path import *
+
 
 logger = logging.getLogger(__name__)
 
@@ -21,11 +24,16 @@ class Aiapi(object):
         self.conf = None
         self.config = {}
         self.cors_origins = "*"
-        
+        self.router=[]
         
         if self.root_path is None:
-            raise "You need to add the config file path!!!"
+            raise "You need to add the config file name!!!"
+        
+        check_path()
+        
         self._make_config()
+        self.rebuild_router()
+        
         self._configure_logging()
         reactor.suggestThreadPoolSize(16)
 
@@ -34,7 +42,13 @@ class Aiapi(object):
                             level=self.config["log_level"])
         logging.captureWarnings(True)
     
-        
+    
+    def check_path(self):
+        if self.config.get("predict_class_path"):
+            pass
+        else:
+            raise "Can't find predict_class_path o(╥﹏╥)o"
+    
     def _make_config(self):
         conf = configparser.ConfigParser()
         conf.read(self.root_path + '/' +self.config_name)
@@ -43,7 +57,11 @@ class Aiapi(object):
         for key in conf.options("default"):         
             self.config[key] = conf.get("default",key)
         
+    def rebuild_router(self):
+        if self.config.get("predict_class") or self.config.get("predict_func"):
+            self.router.append("/parse")
         
+            
     def run(self):     
         self.app.run(self.config["host"],int(self.config['port']))
     
@@ -51,12 +69,16 @@ class Aiapi(object):
     @check_cors
     def hello(self,request):
         logger.info(request.getClientIP())
-        print(dir(request),request)
-        print("Hello~ by clannadhh")
-        return "Hello~ by clannadhh"
+        return "Hello~ by clannadhh (*^▽^*)"
     
     @app.route("/parse",methods=['GET','POST','OPTIONS'])
     @check_cors
+    @check_route
     def parse(self,request):
         return "parse"
+        
+    @app.route("/version",methods=['GET','OPTIONS'])
+    @check_cors
+    def parse(self,request):
+        return "<h2>Welcome!!! ai-web_0.0.1<h2>"
     
--- aiweb/utils/ai_wraper.py
+++ aiweb/utils/ai_wraper.py
@@ -1,5 +1,6 @@
 from functools import wraps
 
+
 def check_cors(f):
     """Wraps a request handler with CORS headers checking."""
 
@@ -23,4 +24,23 @@ def check_cors(f):
         else:
             return f(*args, **kwargs)
 
+    return decorated
+    
+
+def check_route(f):
+    """Wraps a request handler with router."""
+    
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        self = args[0]
+        request = args[1]
+        url = request.uri.decode()
+        html_404 = '''<title>404 Not Found</title>
+                    <h1>Hey!Easy~</h1>
+                    <p>If you see this page,it means you have to add the <span style="color:red;font-size:20px">PREDICT_CLASS </span>and <span style="color:red;font-size:20px">PREDICT_FUNC</span> in you <span style="color:blue;font-size:20px">config.py</span>.</p>'''
+        if url not in self.router:
+            request.setResponseCode(404)
+            return html_404
+        else:
+            return f(*args, **kwargs)
     return decorated
